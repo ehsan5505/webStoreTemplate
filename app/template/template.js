fd6d73639fbd613e6d552763f8ed5cb420d77a40
Ehsan Rafeeque <dreamer_2142@yahoo.com>
2015-12-29 01:13:54 +0000

    web template store v1.0

--- app/template/template.js
+++ app/template/template.js
@@ -1,17 +1,34 @@
 
-
-
 angular.module('ess.templates', ['ngRoute'])
 
 .config(['$routeProvider',function($routeProvider){
-    $routeProvider.when('/template',{
+    
+    $routeProvider
+    .when('/template',{
         templateUrl : 'template/template.html',
         controller : 'tempCntr'
-    });
+    })
+    .when('/template-detail/:id',{
+        templateUrl:'template/template-detail.html',
+        controller:'detailCntr'
+    })
 }])
-.controller('tempCntr',[function(){
-    console.log("There is a issue");
-}]);
+.controller('tempCntr',['$scope','$http',function($scope,$http){
+    // delete $http.defaults.headers.common['X-Requested-With'];
+    
+    $http.get("https://web-template-ehsan55051.c9users.io/app/json/temp.json").success(function(data){
+        $scope.templates = data;
+    })  
+}])
+.controller('detailCntr',['$scope','$http','$routeParams','$filter',function($scope,$http,$routeParams,$filter){
+    $param = $routeParams.id;
+    $http.get("https://web-template-ehsan55051.c9users.io/app/json/temp.json").success(function(data){
+            $scope.template = $filter('filter')(data,function(d){
+                return d.id == $param;
+            })[0];
+    console.log($scope.template);
+    })
+}])
 
 // 'use strict';
 
